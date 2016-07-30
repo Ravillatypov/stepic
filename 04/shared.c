5e87980bbb4c236e8ddea751f5d67e606ab6e2ff
Ravillatypov <ravillatypov12@gmail.com>
2016-07-30 16:43:43 +0300

    add 04 dir

---
+++ 04/shared.c
@@ -0,0 +1,12 @@
+#ifndef SOLUTION
+#define SOLUTION
+
+int someSecretFunctionPrototype(int multiplier);
+
+#endif
+
+int someSecretFunctionPrototype(int multiplier)
+{
+	int r = multiplier * 2;
+	return r;
+}
---
+++ 04/solution.c
@@ -0,0 +1,34 @@
+#include <dlfcn.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int (*func)( int);
+bool init_library(char *libName, char *funcName){
+	void *hdl = dlopen(libName, RTLD_LAZY);
+	if(NULL == hdl)
+		return false;
+
+	func = (int (*) (int))dlsym(hdl, funcName);
+	if (NULL == func)
+		return false;
+
+	return true;
+}
+
+int main(int argc, char *argv[])
+{
+	int q, r = 0;
+	if(argc != 4){
+		printf("a few or many parameters! argc = %i\n", argc);
+		return 1;
+	}
+	q = atoi(argv[3]);	
+	printf("%s - %s - %s\n", argv[1], argv[2], argv[3]);
+	printf("%i\n", q);
+	if(init_library(argv[1], argv[2]))
+		r = func(5);
+	printf("%i\n", r);
+	return 0;
+}
